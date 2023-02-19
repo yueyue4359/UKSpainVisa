61b588dc3cc39d74ee6e4042d6babc50a4b74993
Yue Zhu <88612363+yueyue4359@users.noreply.github.com>
2023-02-19 19:57:14 +0000

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -20,6 +20,6 @@ FIXED = 'https://uk.blsspainvisa.com/visa4spain/book-appointment/'
 # 这里添加客户
 # 账号参数: Email, 密码, 链接地址, 中心地点(CENTER_MAN, CENTER_EDN, CENTER_LON), 是否优先(MODE_NORMAL, MODE_FAST), None是启动线程池的默认参数
 No1 = (['zhuyue0806@gmail.com', 'ADnnnn0931!@', 'X6uqpJaiiQ', CENTER_EDN, MODE_FAST], None)
-#No2 = (['secondPersonEmail@xxx.com', '@password@', 'X6Wnq5ms5f', CENTER_EDN, MODE_NORMAL], None)
+No2 = (['secondPersonEmail@xxx.com', '@password@', 'X6Wnq5ms5f', CENTER_EDN, MODE_NORMAL], None)
 # 加入线程池
-USERS = [No1]
+USERS = [No1, No2]
